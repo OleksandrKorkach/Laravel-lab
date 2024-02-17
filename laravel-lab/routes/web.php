dea5cabd73b3fac4c4329b8635f79a4b99be57f5
Korkach Oleksandr <oleksandrkorkachwork@gmail.com>
2024-02-18 01:53:35 +0200

    routing refactoring

--- laravel-lab/routes/web.php
+++ laravel-lab/routes/web.php
@@ -5,32 +5,26 @@ use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\ProjectController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
-
 
 Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
     Route::get('/', [MainController::class, 'index'])->name('index');
-    Route::get('/projects', [ProjectController::class, 'index'])->name('projects');
-    Route::get('projects/create', [ProjectController::class, 'create'])->name('projects.create');
-    Route::get('/projects/{project}', [ProjectController::class, 'get'])->name('projects.get');
-    Route::post('/projects', [ProjectController::class, 'store'])->name('projects.store');
 
-    Route::get('/projects/{project}/add-user', [ProjectController::class, 'getProjectUsersToAdd'])->name('projects.get-project-users-to-add-user');
-    Route::post('/projects/{project}/add-user', [ProjectController::class, 'addUserToProject'])->name('projects.add-user');
+    Route::prefix('profile')->group(function () {
+        Route::get('/', [ProfileController::class, 'edit'])->name('profile.edit');
+        Route::patch('/', [ProfileController::class, 'update'])->name('profile.update');
+        Route::delete('/', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    });
+
+    Route::prefix('projects')->group(function () {
+        Route::post('/add-member', [ProjectController::class, 'addMember'])->name('projects.add-member');
+        Route::delete('/delete-member', [ProjectController::class, 'deleteMember'])->name('projects.delete-member');
+        Route::get('/{projectId}/search-available-users', [ProjectController::class, 'searchAvailableUsers'])->name('projects.search-available-users');
+        Route::post('/{projectId}/store-statuses', [ProjectController::class, 'storeStatuses'])->name('projects.statuses.store');
+    });
+
+    Route::resource('projects', ProjectController::class);
+
 });
 
 require __DIR__.'/auth.php';
